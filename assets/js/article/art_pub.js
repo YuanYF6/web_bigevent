55300b4dc35e3a06232925abf967a26be9836c40
yyf <2575626559@qq.com>
2021-12-06 20:43:40 +0800

    增加了一些注释

--- assets/js/article/art_pub.js
+++ assets/js/article/art_pub.js
@@ -4,6 +4,8 @@ $(function () {
     initCate();
     // 初始化富文本编辑器
     initEditor()
+
+    // 初始化文章类别
     function initCate() {
         $.ajax({
             method: 'GET',
@@ -15,10 +17,12 @@ $(function () {
 
                 var htmlStr = template('tpl-cate', res)
                 $('[name=cate_id]').html(htmlStr);
+                //表单检测不到数据的变化  需要重新渲染
                 form.render();
             }
         })
     }
+    // 点击选择图片即点击了file文件选择框
     $('#btnChooseImage').on('click', function () {
         $('#coverFile').click();
     })
@@ -36,9 +40,10 @@ $(function () {
 
 
     $('#coverFile').on('change', function (e) {
+        // 获取选择的文件
         var files = e.target.files
         if (files.length === 0) {
-            return
+            return;
         }
         var newImgURL = URL.createObjectURL(files[0]);
         $image
@@ -55,6 +60,9 @@ $(function () {
     $('#form-pub').on('submit', function (e) {
         e.preventDefault();
         // 根据 form 表单创建 FormData 对象，会自动将表单数据填充到 FormData 对象中
+        // FormData 对象的使用：
+        // 1.用一些键值对来模拟一系列表单控件：即把form中所有表单元素的name与value组装成
+        // 一个queryString
         var fd = new FormData($(this)[0]);
         // console.log(fd);
         fd.append('state', art_state)
@@ -68,23 +76,26 @@ $(function () {
             })
             .toBlob(function (blob) {       // 将 Canvas 画布上的内容，转化为文件对象
                 // 得到文件对象后，进行后续的操作
+                // 将文件加入fd
                 fd.append('cover_img', blob);
                 publisherArticle(fd);
             })
     })
 
+    // 发布文章的方法，传入一个formData格式的数据
     function publisherArticle(fd) {
         $.ajax({
             method: 'POST',
             url: '/my/article/add',
             data: fd,
-            contentType: false,
-            processData: false,
+            contentType: false,     // jQuery不要去处理发送的数据
+            processData: false,     // jQuery不要去设置Content-Type请求头
             success: function (res) {
                 if (res.status !== 0) {
                     return layer.msg('发布文字失败');
                 }
                 layer.msg('发布文章成功');
+                // 发布成功后跳转到文章列表页面
                 location.href = '/article/art_list.html'
             }
         })
